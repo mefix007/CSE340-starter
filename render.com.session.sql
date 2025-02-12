e2059993626677f242056d763a22b104c1845394
mefix007 <111250333+mefix007@users.noreply.github.com>
2025-02-12 04:25:24 +0100

    connection string upgraded

--- render.com.session.sql
+++ render.com.session.sql
@@ -1,10 +1,15 @@
 INSERT INTO classification (classification_id, classification_name)
 VALUES (
-        classification_id :integerINSERT INTO classification (classification_id, classification_name)
-        VALUES (
-                classification_id :integer,
-                'classification_name:character varying'
-            );
+                classification_id :integerINSERT INTO classification (classification_id, classification_name)
+                VALUES (
+                                classification_id :integer,
+                                'classification_name:character varying'
+                        );
 ,
 'classification_name:character varying'
 );
+INSERT INTO classification (classification_id, classification_name)
+VALUES (
+                classification_id :integer,
+                'classification_name:character varying'
+        );
